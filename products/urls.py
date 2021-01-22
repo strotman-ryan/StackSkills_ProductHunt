10eafab316d6226fd111b3078c59d668d5874a9c
strotman-ryan <strotman.7@osu.edu>
2021-01-21 23:47:55 -0500

    finsihed the tuorial

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import create
+from .views import create, detail, upvote
 
 urlpatterns = [
-    path('create/', create, name='create')
+    path('create/', create, name='create'),
+    path('<int:product_id>', detail, name='detail'),
+    path('<int:product_id>/upvote', upvote, name='upvote')
 ]
